34513eec5c495fdfc25aa24b05f82ac599a841e5
leonardcad02 <leonardcad02@gmail.com>
2021-07-07 23:08:43 -0500

    Creando el Login

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from os import error
-import re
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from  wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['ENV'] = 'development'
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+bootstrap = Bootstrap(app)
 
 todos = ['Todo 1', 'Todo 2', 'Todo 3']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de Usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit  = SubmitField('Enviar')
+
 
 @app.errorhandler(500)
 
@@ -23,18 +35,20 @@ def index():
     userIp = request.remote_addr
     make = redirect('/hello')
     response = make_response(make)
-    response.set_cookie('userIp',userIp)
-
+    session['userIp'] = userIp
     return response
 
 
 @app.route('/hello')
 def hello():
-    userIp = request.cookies.get('userIp')
+    userIp = session.get('userIp')
+    loginForm = LoginForm()
     context = {
         'userIp': userIp,
         'todos': todos,
+        'loginForm': loginForm,
     }
+
     return render_template('hello.html', **context)
 
 
